e4c672b990be37269fdf6792a0d547d198bab333
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2020-12-05 16:34:55 +0100

    project schema

---
+++ src/services/models/projectModel.js
@@ -0,0 +1,49 @@
+const { Schema } = require("mongoose");
+const mongoose = require("mongoose");
+const v = require("validator");
+const ProjectSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  studentID: {
+    type: Schema.Types.ObjectId,
+    ref: "Student",
+  },
+
+  repoURL: {
+    type: String,
+    //required: true,
+    validate: {
+      validator: (url) => {
+        if (!v.isURL(url)) {
+          throw new Error("url: not valid");
+        }
+      },
+    },
+  },
+
+  liveURL: {
+    type: String,
+    //required: true,
+    validate: {
+      validator: (url) => {
+        if (!v.isURL(url)) {
+          throw new Error("url: not valid");
+        }
+      },
+    },
+  },
+});
+ProjectSchema.static("findAllProjectsByStudentId", async function (id) {
+  let projects = await ProjectModel.find({ studentID: id });
+
+  return projects;
+});
+const ProjectModel = mongoose.model("Project", ProjectSchema);
+
+module.exports = ProjectModel;
